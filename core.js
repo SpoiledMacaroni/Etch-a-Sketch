e2ca7625a682ce6716cc0cd3a65496b3ce9d29ee
SpoiledMacaroni <60438998+SpoiledMacaroni@users.noreply.github.com>
2023-12-27 18:22:39 -0500

    Functional mouseover event Added functional mouseover event so the grid div changes color.

--- core.js
+++ core.js
@@ -5,5 +5,7 @@ for (let i = 1; i < 257; i++) {
     const square = document.createElement('div');
     square.classList.add('square');
     square.textContent = 'x';
+    square.addEventListener("mouseover", function(e) {square.setAttribute("style", "background-color: black;")}, false);
+    square.addEventListener("mouseout",square.setAttribute("style", "background-color: white;"))
     container.appendChild(square);
 }
